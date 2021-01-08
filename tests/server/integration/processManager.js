4ad327855c1221d9638e46d50e2ef79c4d4b1f06
jpw <jon@mdi.eu.com>
2021-01-08 14:17:58 +0000

    tidier

--- tests/server/integration/processManager.js
+++ tests/server/integration/processManager.js
@@ -1,60 +1,53 @@
 'use strict';
 
-var spawn = require('child_process').spawn;
+const spawn = require('child_process').spawn;
 
-var handleEventsForProcess = function (process, label, resolve, reject, allProcessesUp, processesUp) {
+const handleEventsForProcess = function (process, label, resolve, reject, processesUp) {
 
 	function confirmProcessUp (label) {
 		processesUp[label] = true;
-		console.log(label + ' is now '+ processesUp[label])
-		console.log('processesUp.frontendChild is now '+ processesUp.frontendChild)
-		console.log('processesUp.backendChild is now '+ processesUp.backendChild)
-
 		if (processesUp.frontendChild && processesUp.backendChild) {
-			allProcessesUp = true;
-			console.log('resolve '+ label)
 			resolve();
 		}
 	};
 
 	// we cannot tell if a child process has finished spawing (no such event exists),
 	//  so we have to wait for both of them to output something. And pray they aren't
-	//  changed to start silently.
+	//  changed to start silently (unlikely, but if so, the tests will hang).
 	process.stdout.on('data', function (data) {
-		if (!allProcessesUp) {
+		if (!(processesUp.frontendChild && processesUp.backendChild)) {
 			confirmProcessUp(label)
 		};
-
-		console.log(label + ' stdout: ' + data);
+		console.log(`${label} stdout: ${data}`);
 	});
 
 	process.stderr.on('data', function (data) {
-		console.log(label + ' stderr: ' + data);
+		console.log(`${label} stderr: ${data}`);
 	});
 
 	process.on('close', function (data) {
-		console.log(label + ' child process exited with code: ' + data);
+		console.log(`${label} child process exited with code: ${data}`);
 		reject();
 	});
 }
 
-// SIGINT to gracefulyl clsoe
+// SIGINT to gracefully close
 module.exports = {
 	startServers: function() {
 		return new Promise(function (resolve, reject) {
-			var allProcessesUp = false;
-			var processesUp = {
+			// we need to resolve() when all processes are up, so share these refs between children
+			let processesUp = {
 				frontendChild: false,
 				backendChild: false
 			}
 
-			var backendChild = spawn('node', ['./bin/serve.js']);
-			handleEventsForProcess(backendChild, 'backendChild', resolve, reject, allProcessesUp, processesUp);
+			const backendChild = spawn('node', ['./bin/serve.js']);
+			handleEventsForProcess(backendChild, 'backendChild', resolve, reject, processesUp);
 
-			var frontendChild = spawn('node', ['app', '-c', '1'], {
+			const frontendChild = spawn('node', ['app', '-c', '1'], {
 				cwd: 'tests/mock-app/'
 			});
-			handleEventsForProcess(frontendChild, 'frontendChild', resolve, reject, allProcessesUp, processesUp);
+			handleEventsForProcess(frontendChild, 'frontendChild', resolve, reject, processesUp);
 		});
 	}
 }
