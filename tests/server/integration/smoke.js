c53b69fd3d57e52c11280fa24a53eb6bd592b764
jpw <jon@mdi.eu.com>
2021-01-18 12:24:44 +0000

    mocha now waits for smoke test result

--- tests/server/integration/smoke.js
+++ tests/server/integration/smoke.js
@@ -1,33 +1,47 @@
 const request = require('supertest');
+const assert = require('proclaim');
+
 const serversUnderTest = require('./lib/serversUnderTest');
-/*
-// wait for the servers, run the tests, then cleanup
-const serversReadyPromise = serversUnderTest.readyForTest();
-serversReadyPromise
-	.then(() => {
-		const runTestsPromise = runTests();
-		runTestsPromise
-			.then(() => {
+
+describe('Smoke', () => {
+	let getHomeResponseBody;
+
+	before(done => {
+		// wait for the servers, run the tests, then cleanup
+		const serversReadyPromise = serversUnderTest.readyForTest();
+		serversReadyPromise
+		.then(() => {
+			const getHomePromise = getHome();
+			getHomePromise
+			.then(data => {
+				getHomeResponseBody = data;
 				serversUnderTest.finish();
+				done(); // let mocha know it can now run the suite
 			})
 			.catch(err => {
 				console.error(err);
 				serversUnderTest.finish();
 			})
-	});
+		});
 
-const runTests = () => {
-	return new Promise((resolve, reject) => {
-		const testRequest = request('http://localhost:5400');
-		testRequest
-			.get('/home')
-			.then(res => {
-				console.log(res.text)
-				resolve();
-			})
-			.catch(err => {
-				reject(err)
-			})
+		const getHome = () => {
+			return new Promise((resolve, reject) => {
+				const testRequest = request('http://localhost:5400');
+				testRequest
+				.get('/home')
+				.then(res => {
+					console.log(res.text)
+					resolve(res.text);
+				})
+				.catch(err => {
+					reject(err)
+				})
+			});
+		}
+	}); // before
+
+	it('Should return hello world text in response', () => {
+		console.log(getHomeResponseBody)
+		assert.isTrue(getHomeResponseBody === 'ff');
 	});
-}
-*/
+});
