b7470ac9071cff6b1734ceea3fb7db3b67426a1c
jpw <jpw@users.noreply.github.com>
2021-01-19 15:32:47 +0000

    V4 hotifx (#339)

---
+++ tests/mock-app/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var shunter = require('../../');
+var app = shunter({
+    path: {
+        themes: __dirname
+    },
+    routes: {
+        localhost: {
+            default: {
+                host: '127.0.0.1',
+                port: 5401
+            }
+        }
+    }
+
+});
+
+app.start();
+console.log('mock-app started')
