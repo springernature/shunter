2644f23f2e9091bb9ba8daf232f7c6a921e09cea
jpw <jon@mdi.eu.com>
2021-01-19 08:53:50 +0000

    adds node 4 compat

--- tests/server/integration/lib/serversUnderTest.js
+++ tests/server/integration/lib/serversUnderTest.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const spawn = require('child_process').spawn;
-const request = require('supertest');
+var spawn = require('child_process').spawn;
+var request = require('supertest');
 
-let processes = new Array();
-let allProcessesUp = false;
+var processes = new Array();
+var allProcessesUp = false;
 
 // starts the fe and be servers
 // returns a Promise which resolves when both have output to their STDOUTs
-const startServers = () => {
-	return new Promise((resolve, reject) => {
-		let backend = spawn('node', ['../../bin/serve.js'], {
+var startServers = function () {
+	return new Promise(function (resolve, reject) {
+		var backend = spawn('node', ['../../bin/serve.js'], {
 			cwd: 'tests/mock-app/'
 		});
 		processes.push(backend);
@@ -18,7 +18,7 @@ const startServers = () => {
 
 		// start the FE with one worker process (-c 1), and compile the templates on demand
 		//  to minimise the time between server up and server able to handle reqs
-		let frontend = spawn('node', ['app', '-c', '1', '--compile-on-demand', 'true'], {
+		var frontend = spawn('node', ['app', '-c', '1', '--compile-on-demand', 'true'], {
 			cwd: 'tests/mock-app/'
 		});
 		processes.push(frontend);
@@ -27,62 +27,57 @@ const startServers = () => {
 };
 
 // handles events common for both server processes, on stderr stdout etc.
-const handleEventsForProcess = (process, resolve, reject) => {
+var handleEventsForProcess = function (process, resolve, reject) {
 	// console.log(`Spawned child pid: ${process.pid}`);
-	process.isUp = false;
+	process.__isUp = false;
 
 	// we cannot tell if a child process has finished spawing (no such event exists),
 	//  so we have to wait for both of them to output something. And pray they aren't
 	//  changed to start silently (unlikely, but if so, the tests will hang).
-	process.stdout.on('data', (data) => {
-		console.log(`${process.pid} stdout: ${data}`);
+	process.stdout.on('data', function (data) {
+		// console.log(`${process.pid} stdout: ${data}`);
 		if (allProcessesUp) {
 			return;
 		}
 
-		process.isUp = true;
+		process.__isUp = true;
 
-		/*
-		let pc = 0;
-		processes.forEach(process => {
-			console.log(`PROCESSS ${pc} isup ${process.isUp}`);
-			pc++
-		})
-		*/
-
-		if (processes.length > 1 && processes.every(process => process.isUp === true)){
+		if (processes.length > 1 &&
+			processes.every(function (process) {
+				return process.__isUp === true
+		})){
 			allProcessesUp = true;
 			resolve();
 		}
 	});
 
-	process.stderr.on('data', data => {
+	process.stderr.on('data', function (data) {
 		console.log(`${process.pid} stderr: ${data}`);
 		reject();
 	});
 
-	process.on('close', data => {
-		console.log(`${process.pid} child process exited with code: ${data}`);
+	process.on('close', function (data) {
+		// console.log(`${process.pid} child process exited with code: ${data}`);
 	});
 }
 
 // ping the FE server
 // returns a Promise which resolves once it recieves a pong response
 //  or rejects on an unexpected error, or if maxTries exceeded
-const serversResponding = () => {
-	return new Promise((resolve, reject) => {
-		let tries = 0;
-		const maxTries = 1000;
-		const thisRequest = request('http://localhost:5400');
-		const doPingLoop = () => {
+var serversResponding = function () {
+	return new Promise(function (resolve, reject) {
+		var tries = 0;
+		var maxTries = 1000;
+		var thisRequest = request('http://localhost:5400');
+		var doPingLoop = function () {
 			thisRequest
 				.get('/ping')
-				.then(res => {
+				.then(function (res) {
 					if (res.text === 'pong') {
 						resolve(tries);
 					}
 				})
-				.catch(err => {
+				.catch(function (err) {
 					tries++;
 					if (err.message === 'ECONNREFUSED: Connection refused' && tries < maxTries) {
 						doPingLoop();
@@ -95,9 +90,11 @@ const serversResponding = () => {
 	});
 };
 
-const cleanup = () => {
+var cleanup = function () {
 	try {
-		processes.forEach(process => process.kill('SIGINT'));
+		processes.forEach(function (process) {
+			process.kill('SIGINT')
+		});
 	} catch (err) {
 		console.error(err);
 	}
@@ -106,31 +103,31 @@ const cleanup = () => {
 module.exports = {
 	// starts up the servers, and pings the FE server
 	//  returns a Promise that resolves once the FE pongs back, or crashes and burns
-	readyForTest: () => {
-		return new Promise((resolve, reject) => {
+	readyForTest: function () {
+		return new Promise(function (resolve, reject) {
 			// sequential promises without async/await are not pretty
-			let startServersPromise = startServers();
+			var startServersPromise = startServers();
 			startServersPromise
-				.then(() => {
-					let serversRespondingPromise = serversResponding();
+				.then(function () {
+					var serversRespondingPromise = serversResponding();
 					serversRespondingPromise
-						.then(() => {
+						.then(function () {
 							resolve()
 						})
-						.catch(err => {
+						.catch(function (err) {
 							console.error(err)
 							reject(err);
 							cleanup(); // tear down the test suite if we cant run smoke tests
 						});
 				})
-				.catch(err => {
+				.catch(function (err) {
 					console.error(err)
 					reject(err);
 					cleanup();
 				});
 		})
 	},
-	finish: () => {
+	finish: function () {
 		cleanup();
 	}
 };
--- tests/server/integration/smoke.js
+++ tests/server/integration/smoke.js
@@ -1,45 +1,47 @@
-const request = require('supertest');
-const assert = require('proclaim');
+'use strict';
 
-const serversUnderTest = require('./lib/serversUnderTest');
+var request = require('supertest');
+var assert = require('proclaim');
 
-describe('Smoke', () => {
-	let getHomeResponseBody;
+var serversUnderTest = require('./lib/serversUnderTest');
 
-	before(done => {
+describe('Smoke', function () {
+	var getHomeResponseBody;
+
+	before(function (done) {
 		// wait for the servers, run the tests, then cleanup
-		const serversReadyPromise = serversUnderTest.readyForTest();
+		var serversReadyPromise = serversUnderTest.readyForTest();
 		serversReadyPromise
-		.then(() => {
-			const getHomePromise = getHome();
+		.then(function () {
+			var getHomePromise = getHome();
 			getHomePromise
-			.then(data => {
+			.then(function (data) {
 				getHomeResponseBody = data;
 				serversUnderTest.finish();
-				done(); // let mocha know it can now run the suite
+				done(); // var mocha know it can now run the suite
 			})
-			.catch(err => {
+			.catch(function (err) {
 				console.error(err);
 				serversUnderTest.finish();
 			})
 		});
 
-		const getHome = () => {
-			return new Promise((resolve, reject) => {
-				const testRequest = request('http://localhost:5400');
+		var getHome = function () {
+			return new Promise(function(resolve, reject) {
+				var testRequest = request('http://localhost:5400');
 				testRequest
 				.get('/home')
-				.then(res => {
+				.then(function (res) {
 					resolve(res.text);
 				})
-				.catch(err => {
+				.catch(function (err) {
 					reject(err)
 				})
 			});
 		}
 	}); // before
 
-	it('Should return hello world text in response', () => {
+	it('Should return hello world text in response', function () {
 		assert.isTrue(getHomeResponseBody.includes('<h1>Hello World!</h1>'));
 	});
 });
