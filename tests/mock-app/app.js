4d93d9cd141e6a71431c821636c472e466a19874
jpw <jpw@users.noreply.github.com>
2022-01-07 12:46:15 +0000

    beautiful linting fixes 😍  (#354)

--- tests/mock-app/app.js
+++ tests/mock-app/app.js
@@ -1,20 +1,21 @@
 'use strict';
 
-var shunter = require('../../');
+var shunter = require('../../'); // eslint-disable-line  unicorn/import-index
+
 var app = shunter({
-    path: {
-        themes: __dirname
-    },
-    routes: {
-        localhost: {
-            default: {
-                host: '127.0.0.1',
-                port: 5401
-            }
-        }
-    }
+	path: {
+		themes: __dirname
+	},
+	routes: {
+		localhost: {
+			default: {
+				host: '127.0.0.1',
+				port: 5401
+			}
+		}
+	}
 
 });
 
 app.start();
-console.log('mock-app started')
+console.log('mock-app started');
